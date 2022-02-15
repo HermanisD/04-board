b4bff2713e742718f4e778f126266564634d20fa
Hermanis Djomkins <hermanis.djomkins@gmail.com>
2022-02-15 11:04:40 +0200

    updated

---
+++ app.js
@@ -0,0 +1,26 @@
+const board = document.querySelector("#board")
+const colors = ["#32CD32", "#228B22", "#00FA9A", "#ADFF2F", "#00FF7F"]
+const SQUARES_NUMBER = 500
+
+for (let i=0; i<SQUARES_NUMBER; i++){
+    const square = document.createElement("div")
+    square.classList.add("square")
+
+    square.addEventListener("mouseover", () =>setColor(square))
+    square.addEventListener("mouseleave", () =>removeColor(square))
+    board.append(square)
+}
+
+function setColor(element){
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}` 
+}function removeColor(element){
+    element.style.backgroundColor = "#1d1d1d"
+    element.style.boxShadow = `0 0 2px #000`
+}
+function getRandomColor(){
+    const index=Math.floor(Math.random()*colors.length)
+    return colors[index]
+}
+
